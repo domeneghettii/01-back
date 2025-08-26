559c13952f3d32108f5c0e2b1ef6d87d3a6fea11
Luana Domeneghetti <158210063+domeneghettii@users.noreply.github.com>
2025-08-26 14:30:21 -0300

    feat: create subtrair test.

--- __tests__/calculadora.test.js
+++ __tests__/calculadora.test.js
@@ -1,5 +1,4 @@
-import { somar } from "../src/calculadora.js";
-
+import { somar, subtrair } from "../src/calculadora.js";
 
 describe("Calculadora", () => {
     test("Deve somar dois números corretamente", () => {
@@ -13,6 +12,19 @@ describe("Calculadora", () => {
 
         // Assert (Verificar)
         expect(resultado).toBe(12);
+    });
+    
+    test("Deve subtrair dois números corretamente", () => {
+        // AAA
+        // Arrange (Preparar)
+        const a = 5;
+        const b = 7;
+
+        // Act (Executar)
+        const resultado = subtrair(a, b);
+
+        // Assert (Verificar)
+        expect(resultado).toBe(-2);
     });
     
 });
--- src/calculadora.js
+++ src/calculadora.js
@@ -2,3 +2,9 @@
 export function somar(a, b) {
     return a + b;
 }
+
+// Função para testar subtração
+export function subtrair(a,b) {
+    return a - b;
+}
+
